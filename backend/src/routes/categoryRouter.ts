3cc0af61d6d2db4f69c0d3440b1fdd05fe4da1e8
R3ndrex <yacholohchtoli@gmail.com>
2026-05-24 16:20:07 +0300

    remove backend

--- backend/src/routes/categoryRouter.ts
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import categoryController from "../controllers/categoryController.js";
-const categoryRouter: Router = Router();
-
-categoryRouter.get("/", categoryController.getAll);
-export default categoryRouter;
