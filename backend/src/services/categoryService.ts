8571944294614060b8c75efab36b89d9740abeae
R3ndrex <yacholohchtoli@gmail.com>
2026-05-24 15:11:12 +0300

    add checkamount

--- backend/src/services/categoryService.ts
+++ backend/src/services/categoryService.ts
@@ -1,8 +1,24 @@
+import ApiError from "../error/ApiError.js";
 import { prisma } from "../lib/prisma.js";
 
+const MAX_TODOS_PER_CATEGORY = 5;
 class CategoryService {
     async getAll() {
         return await prisma.category.findMany({});
+    }
+    async checkAmount(categoryId: string) {
+        const todosInCategory = await prisma.category.findUnique({
+            where: {
+                id: categoryId,
+            },
+            include: {
+                tasks: true,
+            },
+        });
+        if (!todosInCategory) {
+            throw ApiError.BadRequest();
+        }
+        return MAX_TODOS_PER_CATEGORY >= todosInCategory?.tasks.length;
     }
 }
 export default new CategoryService();
