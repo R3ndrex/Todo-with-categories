320503de9d0e7edf3cfb661dfaee08c5ba87d216
R3ndrex <yacholohchtoli@gmail.com>
2026-05-24 15:04:32 +0300

    add validation

---
+++ backend/src/validators/todosValidator.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+export const paramsTodoSchema = z.object({
+    params: z.object({
+        id: z.uuid(),
+    }),
+});
+export const createTodoSchema = z.object({
+    categoryId: z.uuid(),
+    name: z.string(),
+});
